26d609647ecbea0d2ff2bd0e3d7b8813dbcc712c
pal-16 <50794716+pal-16@users.noreply.github.com>
2022-03-26 03:24:22 +0530

    post controller

--- new_server/src/controllers/PostController.js
+++ new_server/src/controllers/PostController.js
@@ -13,8 +13,32 @@ exports.createPost = async (req, res, next) => {
 
 exports.getAllPosts = async (req, res, next) => {
     try{
+
         const posts = await Post.find().populate('comments').populate('poster');
         console.log(posts);
+        let category=[];
+        for (let i = 0; i < posts.length; i++) {
+         category.push(posts[i].category);
+        }
+        console.log(category);
+
+        const response = await axios.post(
+          "https://b3d4-2405-204-287-dba-7975-4dc3-4c21-bf35.ngrok.io",
+          {
+            user_id:req.body.userId,
+            postCategory:category
+          },
+          {
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        );
+       
+        await Post.findAndUpdate({
+          $push: { displayRank:response}
+        });
+      
         res.status(200).json({data: posts, message: 'all posts'});
     } catch(error) {
         return res.status(500).json({ error: error.message });
