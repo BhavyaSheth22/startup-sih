cac370bb159a3ba9861ce82fa45f63ddc125ea39
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2022-03-25 20:20:18 +0530

    added posts/comments/workshops crud

--- new_server/src/controllers/PostController.js
+++ new_server/src/controllers/PostController.js
@@ -1,21 +1,22 @@
-const postModel = require("../models/Post");
+const {Post} = require("../models/Post");
 
 exports.createPost = async (req, res, next) => {
     try {
       const postData = req.body;
-      const createPostData = await postModel.create({ ...postData })
+      const createPostData = await Post.create({ ...postData })
 
       res.status(201).json({ data: createPostData, message: 'created' });
     } catch (error) {
-      return res.status(500).json({ error: e.message });
+      return res.status(500).json({ error: error.message });
     }
   };
 
 exports.getAllPosts = async (req, res, next) => {
     try{
-        const posts = postModel.find();
+        const posts = await Post.find().populate('comments').populate('poster');
+        console.log(posts);
         res.status(200).json({data: posts, message: 'all posts'});
     } catch(error) {
-        return res.status(500).json({ error: e.message });
+        return res.status(500).json({ error: error.message });
     }
 }
