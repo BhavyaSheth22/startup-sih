50ae5823135e31c3f24b4a011aa2d85588161276
pal-16 <50794716+pal-16@users.noreply.github.com>
2022-03-26 02:21:36 +0530

    added incubator

--- new_client/src/pages/Home.js
+++ new_client/src/pages/Home.js
@@ -65,7 +65,7 @@ const Home = ({ isAuthenticated, setIsAuthenticated }) => {
   ];
   return (
     <>
-      <section className="text-gray-700 body-font lg:mx-16 sm:mx-4">
+      <section className="text-gray-700 body-font lg:mx-16 sm:mx-2">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
             <img
